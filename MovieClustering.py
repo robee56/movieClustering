81cef1dfbfbddc9a47e161d154c567a79221c0b1
robinJo <robin.joganah@gmail.com>
2015-04-07 12:14:46 -0400

    implémentation kmean

--- MovieClustering.py
+++ MovieClustering.py
@@ -2,7 +2,10 @@ from dataset import Dataset
 from movies import Movie
 from notes import Notes
 from users import User
+from kmean import kMeans
 import os
+from numpy import *
+import numpy
 
 currentPath = os.path.dirname(os.path.realpath(__file__))
 
@@ -11,5 +14,32 @@ FileMovies = currentPath  + "/data/u.item"
 FileNotes = currentPath  + "/data/u.base"
 
 dataset = Dataset(FileUser,FileMovies,FileNotes)
-for i in dataset.users:
-	print i.profession
+# reducedDataset = []
+reducedDataset = numpy.zeros((len(dataset.users),2))
+
+for i in range(len(dataset.users)):
+	# if(dataset.users[i].genre == "M"):
+	# 	genre = 0
+	# else:
+	# 	genre = 1
+	# reducedDataset.append((dataset.users[i].age,genre))
+
+	for j in range(2):
+
+		if(j==0):
+			reducedDataset[i,j] = dataset.users[i].age
+		else:
+			if(dataset.users[i].genre == "M"):
+				genre = 0
+			else:
+				genre = 1
+			reducedDataset[i,j] = genre 
+
+
+print 'new dataset'
+print reducedDataset[0]
+m = shape(reducedDataset)[0]
+print reducedDataset[0,1]
+
+print m
+kMeans(reducedDataset,3)
--- clustering.py
+++ clustering.py
@@ -1,9 +1,38 @@
+from numpy import *
 class Clustering:
 	def __init__(self,data):
 		print 'kmeans : '
 		self.kmeans(self,data)
 		print 'GA : '
 		self.geneticAlgorithm(self,data)
+
+	def intersect(self,a, b):
+		return list(set(a) & set(b))
+	def pcs(self,x,y,notes):
+		listallX=[]
+		listallnotesX =[]
+		listY=[]
+		listX=[]
+		i=0
+		for note in notes:
+			if(note.idUser == x.id):
+				listallX.append(note.idMovie)
+				listallnotesX.append(note)
+
+
+		for note in notes:
+			dansY = False
+			dansX = False
+			if(note.idMovie in listallX):
+				dansX = True
+			if(note.idUser == y.id):
+				dansY = True
+			if(dansX and dansY)
+				list.append(note in listallnotesX if listallnotesX.)
+				listY.append(note)
+		listX = 
+
+
 	def kmeans(self,data):
 		print "not implemented"
 	def geneticAlgorithm(self,data):
